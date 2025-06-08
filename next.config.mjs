747a3b6ad063643bfd7646ffd5941a8ab43a8e8a
liqnerd <polaklxx@gmail.com>
2025-06-08 19:27:47 +0200

    Add GitHub Pages configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,1 +1,10 @@
- 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/luciegerova',
+  images: {
+    unoptimized: true,
+  },
+}
+
+export default nextConfig 
